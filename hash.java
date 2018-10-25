2e18621d9c8652718bbcd1027ace9dcff7ce706d
muhammed özbilici <mahmut716@gmail.com>
2018-10-25 16:56:12 +0300

    Update hash.java

--- hash.java
+++ hash.java
@@ -25,7 +25,7 @@
             byte[] thedigest;
             MessageDigest md = MessageDigest.getInstance("SHA-1");
             md.reset();
-            md.update(("<Trh=13/06/2018/><SN=0341218/><AlKK=0029/><AlSK=99999/><GonKK=0098/><GonSK=99999/><IslTur=M02/><Ttr=126000.00/><Onc=2/><AlAd=TEST TEST TEST İDRİS/><GonAd=JPMORGAN CHASE BANK, N.A. MERKEZI COLUM/><GonKimN=00000000000/><OdmKynk=I/><Acklm=ILKE TEST 2/>2018061300980341218").getBytes());
+            md.update((hshMsg).getBytes());
             thedigest = md.digest();
             for (byte b : thedigest) {
                 sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
